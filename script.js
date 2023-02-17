c3bd3f0d036828653837f354dcb0fb244e712226
Goodness Gibendi <71548390+Double-gg123@users.noreply.github.com>
2023-02-17 20:42:22 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,33 @@
+// Write a function called add7 that takes one number and returns that number + 7.
+// function add7(number) {
+//     return number + 7;
+// }
+// console.log(add7(7)); 
+
+// function to return multiplication of 2 number
+// function multiply(num1,num2){
+//   return num1*num2;
+// }
+// const result = multiply(5, 6);
+// console.log(result);
+
+
+// Write a function called capitalize that takes a string and returns that string with only the first letter capitalized. Make sure that it can take strings that are lowercase, UPPERCASE or BoTh.
+
+// function capitalize(str) {
+//   return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+// }
+// const result1 = capitalize("hello"); // "Hello"
+// const result2 = capitalize("WORLD"); // "World"
+// const result3 = capitalize("sOmE tExT"); // "Some text"
+// console.log(result1);
+// console.log(result2);
+// console.log(result3);
+
+
+// function lastLetter(str) {
+//   return str.charAt(str.length - 1);
+// }
+// const str = "abcd";
+// const lastChar = lastLetter(str);
+// console.log(lastChar); // outputs "d"
